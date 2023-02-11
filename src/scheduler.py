013e439b621d9bda41f7c30940ae37ec08f458fa
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-02-11 19:01:14 +0900

    データベースから取得したURLをもとに、検出を行う処理を追加

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,13 +1,60 @@
+import os
 import shutil
+import pafy
+import cv2
+import datetime
 import subprocess
 from subprocess import PIPE
+import mysql.connector
+from dotenv import load_dotenv
 
-def detect_yolo():
-    proc = subprocess.run(['python', 'yolov5/detect.py'], stdout=PIPE, stderr=PIPE)
-    proc_str = proc.stdout.decode('utf-8').split()
+load_dotenv()
+CONNECTION = mysql.connector.connect(
+    host = os.getenv("MYSQL_HOST"),
+    user = os.getenv("MYSQL_USER"),
+    password = os.getenv("MYSQL_PASSWORD"),
+    database = os.getenv("MYSQL_DATABASE"))
+
+def select_databese():
+    cursor = CONNECTION.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM spots")
+    spot_data = cursor.fetchall()
+    CONNECTION.commit()
+    cursor.close()
+
+    return spot_data
+
+def extract_image(spot_data):
+    dir_path = 'yolov5/data/images/'
+    basename = 'camera_capture_cycle'
+    ext = 'jpg'
+
+    if os.path.isdir('yolov5/data/images/'):
+        shutil.rmtree('yolov5/data/images/')
+
+    for i in range(len(spot_data)):
+        video = pafy.new(spot_data[i]["spots_url"])
+        best = video.getbest(preftype="mp4")
+        cap = cv2.VideoCapture(best.url)
+            
+        if not cap.isOpened():
+            return
+
+        os.makedirs(dir_path, exist_ok=True)
+        base_path = os.path.join(dir_path, basename)
+        ret, frame = cap.read()
+        cv2.imwrite('{}_{}.{}'.format(base_path, datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'), ext), frame)
+
+def detect_image():
+    proc = subprocess.run(["python", "yolov5/detect.py"], stdout=PIPE, stderr=PIPE)
+    proc_str = proc.stdout.decode("utf-8").split()
     proc_int = [int(i) for i in proc_str]
-    shutil.rmtree('yolov5/runs')
-
+    shutil.rmtree("yolov5/runs")
     print(proc_int)
 
-detect_yolo() 
+def main():
+    spot_data = select_databese()
+    extract_image(spot_data)
+    detect_image()
+
+main()
