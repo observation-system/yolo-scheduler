7eeeff6a2bf39875204bd791c8c79ba30392d671
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-02-11 15:22:53 +0900

    検出後の画像を削除する処理を追加

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,3 +1,4 @@
+import shutil
 import subprocess
 from subprocess import PIPE
 
@@ -5,6 +6,8 @@ def detect_yolo():
     proc = subprocess.run(['python', 'yolov5/detect.py'], stdout=PIPE, stderr=PIPE)
     proc_str = proc.stdout.decode('utf-8').split()
     proc_int = [int(i) for i in proc_str]
+    shutil.rmtree('yolov5/runs')
+
     print(proc_int)
 
 detect_yolo() 
